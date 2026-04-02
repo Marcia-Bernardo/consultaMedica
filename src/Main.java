26a546dc28e2f04817ce4c2536020483f4b915eb
Márcia Regina Bernardo <marciabernardo@ipvc.pt>
2026-04-02 13:43:39 +0100

    Modifiquei retirei redundancia

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sucesso");
         System.out.println("Login");
-        System.out.println("Criado com sucesso!");
+
     }
 }
